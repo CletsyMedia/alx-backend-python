19bb6c9bb9ad36be6cab7a685c16a788bf4a5ff0
CletsyMedia <Cletussam12@yahoo.com>
2024-05-03 00:20:26 +0100

    Basic annotations-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for the floor function
+"""
+
+
+import math
+
+
+def floor(x: float) -> int:
+    """
+    returns the floor of a float
+    """
+    return math.floor(x)
