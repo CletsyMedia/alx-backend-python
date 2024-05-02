db918c1bb6803d838d95737e24e1f0c4425c995a
CletsyMedia <Cletussam12@yahoo.com>
2024-05-03 00:33:00 +0100

    omplex types - list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for sum_list function
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns the sum of all elements in a list
+    """
+    return sum(input_list)
