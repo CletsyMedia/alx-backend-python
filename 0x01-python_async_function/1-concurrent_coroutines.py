b7f9962b3c665ee5feac4b8162f82c18f3938a4d
CletsyMedia <Cletussam12@yahoo.com>
2024-05-06 16:06:51 +0100

    multiple coroutines same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,6 @@ Async routine that spawns wait_random n times with the specified max_delay.
 """
 import asyncio
 from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
