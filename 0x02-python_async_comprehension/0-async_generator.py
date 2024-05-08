80a1f23b229dbaa45c590eb3812f1ec257af67a7
CletsyMedia <Cletussam12@yahoo.com>
2024-05-08 21:13:18 +0100

    Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,9 +13,9 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Asynchronous generator that yields a random number between 0 and 10
     after each second, repeated 10 times.
     """
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
 
 
 if __name__ == "__main__":
