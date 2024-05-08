1a6f854bd08fb27db8cbeeec41d93934b2cb5821
CletsyMedia <Cletussam12@yahoo.com>
2024-05-08 21:18:11 +0100

    Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ Async Generator
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that yields a random number between 0 and 10
     after each second, repeated 10 times.
@@ -16,16 +16,3 @@ async def async_generator() -> AsyncGenerator[float, None]:
     for i in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
-
-
-if __name__ == "__main__":
-    async def print_yielded_values() -> None:
-        """
-        Asynchronous function to print values yielded by the async generator.
-        """
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-
-    asyncio.run(print_yielded_values())
