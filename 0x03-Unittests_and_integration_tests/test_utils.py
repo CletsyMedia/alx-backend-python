d2b29b8f10735872181e0d4b57a233846857d528
CletsyMedia <Cletussam12@yahoo.com>
2024-05-26 01:25:56 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 
 
@@ -59,6 +59,42 @@ class TestGetJson(unittest.TestCase):
 
         # Assert that the result matches the expected payload
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test case for memoize decorator"""
+
+    class TestClass:
+        """Test class for memoization"""
+
+        def a_method(self):
+            """A method returning 42"""
+            return 42
+
+        @memoize
+        def a_property(self):
+            """A memoized property returning the result of a_method"""
+            return self.a_method()
+
+    @patch.object(TestClass, 'a_method')
+    def test_memoize(self, mock_a_method):
+        """Test memoization behavior"""
+        # Mock the method
+        mock_a_method.return_value = 42
+
+        # Create an instance of TestClass
+        test_instance = self.TestClass()
+
+        # Call a_property twice
+        result1 = test_instance.a_property
+        result2 = test_instance.a_property
+
+        # Assert that a_method was only called once
+        mock_a_method.assert_called_once()
+
+        # Assert that the results are correct
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
 
 
 if __name__ == '__main__':
