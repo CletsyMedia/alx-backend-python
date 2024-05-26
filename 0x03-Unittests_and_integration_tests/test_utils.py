436d7f4e0765c8350130d5304816ee629638e88e
CletsyMedia <Cletussam12@yahoo.com>
2024-05-26 01:20:23 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,34 +2,37 @@
 """Unit tests for utils.py"""
 
 import unittest
-from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import get_json
+from utils import access_nested_map
 
 
-class TestGetJson(unittest.TestCase):
-    """Test case for get_json function"""
+class TestAccessNestedMap(unittest.TestCase):
+    """Test case for access_nested_map function"""
 
     @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
+        ({}, ("a",), KeyError, "'a'"),
+        ({"a": 1}, ("a", "b"), KeyError, "'b'"),
     ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """Test get_json function"""
-        # Mock the behavior of requests.get
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
+    def test_access_nested_map_exception(
+            self,
+            nested_map,
+            path,
+            expected_exception,
+            expected_message):
+        """Test access_nested_map function raises KeyError
+        with expected message"""
+        with self.assertRaises(expected_exception) as context:
+            access_nested_map(nested_map, path)
+        self.assertIn(expected_message, str(context.exception))
 
-        # Call the function under test
-        result = get_json(test_url)
-
-        # Assert that requests.get was called exactly once with test_url
-        mock_get.assert_called_once_with(test_url)
-
-        # Assert that the result matches the expected payload
-        self.assertEqual(result, test_payload)
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map function"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
 if __name__ == '__main__':
